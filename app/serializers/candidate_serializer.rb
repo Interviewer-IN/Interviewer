aeeff9d67c0d989788752a0c3c663b8068901890
Vitaliy Kushnareko <>
2017-10-10 19:00:39 +0300

    Fixed candidates API commented serializer

--- app/serializers/candidate_serializer.rb
+++ app/serializers/candidate_serializer.rb
@@ -1,4 +1,4 @@
 class CandidateSerializer < ActiveModel::Serializer
   #TODO wtf text is doing here
-  attributes :id, :age, :experience, :contacts, :text
+  #attributes :id, :age, :experience, :contacts, :text
 end
