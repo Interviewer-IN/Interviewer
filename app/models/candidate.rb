6455d9893e8be16171768d79519be5dc0d65c635
Vitaliy Kushnareko <>
2017-11-07 13:11:26 +0200

    Fixing some crazy trash with commits

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,11 +3,11 @@ class Candidate < ApplicationRecord
   belongs_to :position
   mount_base64_uploader :cv, DocumentUploader
 
-  validates :name, :format => { :with => /\A[a-zA-Z]+\z/, :message => "Only letters allowed" },
+  validates :name, :format => { :with => /\A[a-zа-яA-ZА-Я]+\z/, :message => "Only letters allowed" },
             :presence => true,
             :length => { :in => 2..20 }
 
-  validates :surname, :format => { :with => /\A[a-zA-Z]+\z/, :message => "Only letters allowed" },
+  validates :surname, :format => { :with => /\A[a-zа-яA-ZА-Я]+\z/, :message => "Only letters allowed" },
             :presence => true,
             :length => { :in => 2..20 }
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  validates :title, :format => { :with => /\A[a-zA-Z]+\z/, :message => "Only letters allowed" },
+  validates :title, :format => { :with => /\A[a-zA-Z0-9 ]+\z/, :message => "Only letters allowed" },
             :presence => true, :uniqueness =>  { :case_sensitive => false },
             :length => { :in => 2..35 }
 
