51ba6f310c0f930bacbbfe5070fa07102e50960c
Vitaliy Kushnareko <>
2017-11-15 19:01:37 +0200

    fix for in 210

--- app/controllers/api/v1/feedbacks_controller.rb
+++ app/controllers/api/v1/feedbacks_controller.rb
@@ -3,7 +3,7 @@ module Api
     class FeedbacksController < ApplicationController
 
       def index
-        feedbacks = Feedback.find(params[:interview_id])
+        feedbacks = Feedback.find_by(interview_id: params[:interview_id])
         render json: {status:"SUCCESS", message: "Feedbacks loaded", data: feedbacks}, status: :ok
       end
 
